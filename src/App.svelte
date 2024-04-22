7b1d55d140e83b09caad05f45f252adc7123ae8e
ibalpinar <ilker@balpinar.com>
2024-04-22 23:55:25 +0300

    Fix some format issues

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
-  import CounterDemo from './lib/CounterDemo.svelte' 
+  import CounterDemo from './lib/CounterDemo.svelte'
 </script>
 
 <main>
