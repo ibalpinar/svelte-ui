09c23a3c7b9688aa94ca2b2421f7f0abfae617fa
ibalpinar <ilker@balpinar.com>
2024-04-23 23:42:14 +0300

    Add basic structure of svelte

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+   let name = 'Resumex';
+   let src = 'https://picsum.photos/350/200';
+   let pString = 'This is a <strong>paragraph</strong> string';
+</script>
+
+<img {src} alt={`random image for ${name}`}>
+
+<h1>Hello {name.toUpperCase()}!</h1>
+
+<p>{@html pString}</p>
+
+<style>
+   h1 {
+      color: #333;
+      font-size: 2em;
+      text-align: center;
+   }
+</style>
