c2028b6e0fa0a6789c79f2f1e77fc9db79d9f344
ibalpinar <ilker@balpinar.com>
2024-04-24 23:50:41 +0300

    Add new type of local variable to component

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script>
    let name = 'Resumex';
    let src = 'https://picsum.photos/350/200';
+   let src2 = 'https://picsum.photos/850/120';
    let pString = 'This is a <strong>paragraph</strong> string';
+   let pImg = `This is a test image <img src='${src2}' />`;
 </script>
 
 <img {src} alt={`random image for ${name}`}>
@@ -9,6 +11,8 @@
 <h1>Hello {name.toUpperCase()}!</h1>
 
 <p>{@html pString}</p>
+
+<p>{@html pImg}</p>
 
 <style>
    h1 {
