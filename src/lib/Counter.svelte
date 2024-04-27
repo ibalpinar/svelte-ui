c8310032c02b0eec0506d457d98e664b8e4a2dde
ibalpinar <ilker@balpinar.com>
2024-04-27 23:55:15 +0300

    Add check to method

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,14 +1,19 @@
 <script>
    let count1 = 0
    let count2 = 0
+   let warningStr = `Warning: You are about to exceed the limit of 10 clicks`;
    function increment() {
-      count1 += 1
+      if (count1 >= 10)
+         return;
+      count1 += 1;
    }
    function increment2() {
-      count2 += 1
+      count2 += 1;
    }
 </script>
 <button on:click={increment}>Clicks {count1}</button>
+<h2>{count1}</h2>
+<h3>{count1 > 10 ? warningStr : ''}</h3>
 <button on:click={increment2}>Clicks {count2}</button>
 
 <style>
